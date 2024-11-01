97f9a235308451a6f5f3aa65e88f666a737f6b2b
Sura <sk@sura.me>
2024-11-01 09:52:44 -0400

    added title

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,14 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/images/art-logo.png"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
+          <li>All about combining painting & engineering</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
